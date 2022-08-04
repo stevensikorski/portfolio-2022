ddcdf02c8aee5c93d0730ebe6c9dba7456e7baa5
stevensikorski <ssikorski03@gmail.com>
2022-08-03 22:20:07 -0400

    Adjusted script to change tabs text color

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,17 +5,23 @@ window.addEventListener("scroll", function(){
     let height = document.documentElement.offsetHeight;
     let header = document.querySelector(".header");
     let logo = document.querySelector(".logo");
-    let tabs = document.querySelector(".tabs");
+    let tabs = document.querySelectorAll("li a");
 
 
     if (scroll < height - padding || scroll > 2 * height - padding && scroll < 3 * height - padding)
     {
         logo.style.filter = "invert(0)";
-        tabs.style.filter = "invert(0)";
+        for (let i = tabs.length; i--;)
+        {
+            tabs[i].style.color = "rgb(255, 255, 255)";
+        }
     }
     else
     {
         logo.style.filter = "invert(1)";
-        tabs.style.filter = "invert(1)";
+        for (let i = tabs.length; i--;)
+        {
+            tabs[i].style.color = "rgb(0, 0, 0)";
+        }
     }
 })
