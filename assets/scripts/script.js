eed199db7d36ba1cac272a8389990341a4d184c0
stevensikorski <ssikorski03@gmail.com>
2022-08-03 22:07:55 -0400

    Small update to stylesheet and rewritten script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,17 +3,19 @@ window.addEventListener("scroll", function(){
 
     let scroll = this.scrollY;
     let height = document.documentElement.offsetHeight;
+    let header = document.querySelector(".header");
     let logo = document.querySelector(".logo");
     let tabs = document.querySelector(".tabs");
 
-    if ((scroll > height - padding && scroll < 2 * height - padding) || (scroll > 3 * height - padding && scroll < 4 * height - padding))
-    {
-        logo.style.filter = "invert(1)";
-        tabs.style.filter = "invert(1)";
-    }
-    else
+
+    if (scroll < height - padding || scroll > 2 * height - padding && scroll < 3 * height - padding)
     {
         logo.style.filter = "invert(0)";
         tabs.style.filter = "invert(0)";
+    }
+    else
+    {
+        logo.style.filter = "invert(1)";
+        tabs.style.filter = "invert(1)";
     }
 })
