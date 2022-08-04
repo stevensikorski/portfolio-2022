95f68575d656bc7f88549995c9542be23ae89065
stevensikorski <ssikorski03@gmail.com>
2022-08-03 20:58:59 -0400

    Added sections, changed design of landing page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,19 @@
 window.addEventListener("scroll", function(){
-    let scroll = this.scrollY;
-    let logo = document.querySelector(".logo");
-    let height = document.documentElement.offsetHeight;
-    console.log(height, scroll);
+    const padding = 16;
 
-    if (scroll > height - 16)
+    let scroll = this.scrollY;
+    let height = document.documentElement.offsetHeight;
+    let logo = document.querySelector(".logo");
+    let tabs = document.querySelector(".tabs");
+
+    if ((scroll > height - padding && scroll < 2 * height - padding) || (scroll > 3 * height - padding && scroll < 4 * height - padding))
     {
         logo.style.filter = "invert(1)";
+        tabs.style.filter = "invert(1)";
     }
     else
     {
         logo.style.filter = "invert(0)";
+        tabs.style.filter = "invert(0)";
     }
 })
