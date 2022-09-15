6eb67b23671885ef246677d20c09d642d3603bf2
stevensikorski <ssikorski03@gmail.com>
2022-09-15 17:24:44 -0400

    Attempted fix for mobile navbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function(){
     const padding = 16;
 
     let scroll = window.scrollY;
-    let height = document.documentElement.offsetHeight;
+    let height = window.screen.height;
     let logo = document.querySelector(".logo");
     let links = document.querySelectorAll(".links");
     let tabs = document.querySelectorAll("li a");
@@ -10,7 +10,6 @@ window.addEventListener("scroll", function(){
     if (scroll < height - padding || scroll > 2 * height - padding && scroll < 3 * height - padding)
     {
         logo.style.filter = "invert(0)";
-
         for (let i = links.length; i--;)
         {
             links[i].style.filter = "invert(0)";
